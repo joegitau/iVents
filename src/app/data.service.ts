e76ff747cdaffed1cc798ced4733032bf2c8bdc5
joseph.gitau <joseph.gitau@canter.fi>
2020-02-21 14:53:12 +0200

    add routing to event-details-component

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 
-import { Event } from "./event";
+import { Event } from "./event.model";
 
 @Injectable({
   providedIn: "root"
--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
 
 import { DataService } from "../data.service";
+import { switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: "app-event-details",
   templateUrl: "./event-details.component.html",
   styleUrls: ["./event-details.component.scss"]
 })
 export class EventDetailsComponent implements OnInit {
   event: any;
-  constructor(private events: DataService, private route: ActivatedRoute) {}
+
+  constructor(private events: DataService, private route: ActivatedRoute, private router: Router) {}
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.params["id"]; // coreced into a number using '+'
-    this.events.fetchEvent(id).subscribe(event => (event = event));
+    // const id = +this.route.snapshot.params["id"]; // coreced into a number using '+'
+    // this.events.fetchEvent(id).subscribe(event => (event = event));
+
+   this.route.paramMap.subscribe( params => {
+     let id = +params.get('id');
+     this.events.fetchEvent(id).subscribe( event => this.event = event);
+
+     console.log(id)
+     console.log(this.event)
+   });
+
+
   }
 }
