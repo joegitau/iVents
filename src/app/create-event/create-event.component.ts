2d295f8886c35b39d2630c204959a9902ef7bf0e
Joe Karanka <joekaranka@Joes-iMac.local>
2020-02-22 22:59:23 +0200

    logic to cancel event creation

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-create-event',
-  templateUrl: './create-event.component.html',
-  styleUrls: ['./create-event.component.scss']
+  selector: "app-create-event",
+  templateUrl: "./create-event.component.html",
+  styleUrls: ["./create-event.component.scss"]
 })
 export class CreateEventComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  cancelEventCreation() {
+    return this.router.navigate(["/events"]);
   }
-
 }
