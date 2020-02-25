babc9abef2a6238a564564fb608e6db978f34dd8
Joe Karanka <joekaranka@Joes-iMac.local>
2020-02-25 23:54:08 +0200

    add login page

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { UserComponent } from "./user.component";
 import { ProfileComponent } from "./profile/profile.component";
+import { LoginComponent } from "./login/login.component";
 
 const routes: Routes = [
   {
     path: "",
     component: UserComponent,
-    children: [{ path: "profile", component: ProfileComponent }]
+    children: [
+      { path: "profile", component: ProfileComponent },
+      { path: "login", component: LoginComponent }
+    ]
   }
 ];
 
