a3b3e785fd5f73ea82b25761724ee20df2a26ee7
joseph.gitau <joseph.gitau@canter.fi>
2020-02-26 10:06:14 +0200

    merge conflicts

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-
-=======
 import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
->>>>>>> 24bfe95881eec453be3dadde2b3a489f2822e298
 
 @Component({
   selector: "app-create-event",
@@ -15,20 +9,9 @@ import { Router } from "@angular/router";
 export class CreateEventComponent implements OnInit {
   constructor(private router: Router) {}
 
-<<<<<<< HEAD
-  constructor(private router: Router) { }
-=======
   ngOnInit(): void {}
->>>>>>> 24bfe95881eec453be3dadde2b3a489f2822e298
 
   cancelEventCreation() {
     return this.router.navigate(["/events"]);
   }
-<<<<<<< HEAD
-
-  cancelEventCreation() {
-    this.router.navigate(['/events'])
-  }
-=======
->>>>>>> 24bfe95881eec453be3dadde2b3a489f2822e298
 }
--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -10,15 +10,6 @@ import { DataService } from "../data.service";
 export class EventDetailsComponent implements OnInit {
   event: any;
 
-<<<<<<< HEAD
-  constructor(private eventsService: DataService, private route: ActivatedRoute, private router: Router) {}
-
-  ngOnInit(): void {
-   this.route.paramMap.subscribe( params => {
-     let id = +params.get('id'); // coerce into an integer
-     this.eventsService.fetchEvent(id).subscribe( event => this.event = event);
-   });
-=======
   constructor(
     private eventsService: DataService,
     private route: ActivatedRoute,
@@ -31,6 +22,5 @@ export class EventDetailsComponent implements OnInit {
       // this.events.fetchEvent(id).subscribe( event => this.event = event);
       this.eventsService.getEvent(id).subscribe(event => (this.event = event));
     });
->>>>>>> 24bfe95881eec453be3dadde2b3a489f2822e298
   }
 }
