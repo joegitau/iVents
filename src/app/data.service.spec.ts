141ad28bba2fea8d88a409a9b258dfb493b6c22a
Joe Karanka <joekaranka@Joes-iMac.local>
2020-03-22 14:33:41 +0200

    add unit tests for data service

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,12 +1,72 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
-import { DataService } from './data.service';
+import { IEvent } from "./event.model";
+import { DataService } from "./data.service";
+import { of, pipe } from "rxjs";
+import { map } from "rxjs/operators";
 
-describe('DataService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+// fdescribe("DataService", () => {
+//   let service: DataService;
 
-  it('should be created', () => {
-    const service: DataService = TestBed.get(DataService);
-    expect(service).toBeTruthy();
+//   beforeEach(() => TestBed.configureTestingModule({
+//     imports: [HttpClientTestingModule],
+//     providers: [DataService]
+//   }));
+
+//   it("should be created", () => {
+//     const service: DataService = TestBed.get(DataService);
+//     expect(service).toBeTruthy();
+//   });
+// });
+
+fdescribe("DataService", () => {
+  let service: DataService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [DataService]
+    });
+
+    service = TestBed.inject(DataService);
+  });
+
+  it("should fetch Events", done => {
+    const mockEvents: IEvent[] = [
+      {
+        id: 1,
+        name: "Name",
+        price: 14,
+        date: "22.05.2020",
+        imageUrl: "https://via.placeholder.com/150"
+      }
+    ];
+
+    spyOn(service, "fetchEvents").and.returnValue(of(mockEvents));
+
+    service.fetchEvents().subscribe(events => {
+      expect(events).toEqual(mockEvents);
+
+      done();
+    });
+  });
+
+  it("should fetch an Event via GET", done => {
+    const mockEvent: IEvent = {
+      id: 1,
+      name: "Name",
+      price: 14,
+      date: "22.05.2020",
+      imageUrl: "https://via.placeholder.com/150"
+    };
+
+    spyOn(service, "getEvent").and.returnValue(of(mockEvent));
+
+    service.getEvent(1).subscribe(event => {
+      expect(event).toEqual(mockEvent);
+
+      done();
+    });
   });
 });
